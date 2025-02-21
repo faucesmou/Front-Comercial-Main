b9dc65378dfb8645e212583e729135a330576c6a
devopsandes <gonzalomorresi@gmail.com>
2025-02-21 15:37:27 -0300

    Se agrega nombre de cliente para los casos en que se inicia la conversación con una plantilla.

--- src/paginas/Chat.jsx
+++ src/paginas/Chat.jsx
@@ -399,6 +399,7 @@ const Chat = () => {
             )} */}
             <div className="flex  items-center">
               <div className="flex flex-col h-full justify-center gap-4">
+            
                 {/* {elemento === 'rechazado' && operadores.includes(decoded.id) && ( */}
                 {elemento === 'rechazado'  && (
                   <button onClick={handleClickResidual}>
@@ -416,9 +417,12 @@ const Chat = () => {
                         d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" 
                       />
                     </svg>
+                    
                   </button>
+                  
                 )}
                 <User />
+                
               </div>
             
             
